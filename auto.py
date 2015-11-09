aa501e2bb87e9ab7f15fc00eb047b54246ca0908
Reece Small <rsmall@salisbury6c.ac.uk>
2015-11-09 11:59:15 +0000

    Update auto.py
    Added sides code

--- auto.py
+++ auto.py
@@ -5,6 +5,25 @@ import sys
 
 #def getLeftDist():
 
+def getSideDist(TRIG, ECHO):
+    gpio.setup(TRIG,gpio.OUT)
+    gpio.setup(ECHO,gpio.IN)
+    gpio.output(TRIG, False)
+    #print "Waiting For Sensor To Settle"
+    time.sleep(2)
+    gpio.output(TRIG, True)
+    time.sleep(0.00001)
+    gpio.output(TRIG, False)
+    while gpio.input(ECHO)==0:
+      pulse_start = time.time()
+    while gpio.input(ECHO)==1:
+      pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    #print ("Distance: " + distance + "cm")
+    return distance
+
 def getDistance(sonar):
     gpio.setup(sonar, gpio.OUT)
     # Send 10us pulse to trigger
@@ -43,8 +62,8 @@ lmb = 21
 rmf = 24
 rmb = 26
 sonar = 8
-trigL = 36
-echoL = 38
+trigL = 38
+echoL = 36
 trigR = 35
 echoR = 37
 # runs setup for all the motor pins
