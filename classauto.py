00a6e52894ebed38503a8bb0f25bcfbb89203347
Reece Small <rsmall@salisbury6c.ac.uk>
2015-11-18 11:25:04 +0000

    Update classauto.py

---
+++ classauto.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as gpio
+import time
+import sys
+
+gpio.setmode(gpio.BOARD)
+
+# all the pins we will use
+lmf = 19
+lmb = 21
+rmf = 26
+rmb = 24
+sonar = 8
+trigL = 38
+echoL = 36
+trigR = 37
+echoR = 35
