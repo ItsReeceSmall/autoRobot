b61046f9e176fe6d963cc049b8c1cb3da704a212
Reece Small <rsmall@salisbury6c.ac.uk>
2015-11-30 12:45:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
+import time, os, sys
 import pins
 
-def setup():
+def setup(trigL,trigR,sonar,lmf,lmb,rmf,rmb,echoL,echoR):
     #set GPIO up
-    
+    gpio.setmode(gpio.BOARD)
     #Set up pins from a class
-    i = [1,2,3]
-    o = [4,5,6]
+    i = [trigL,trigR]
+    o = [sonar,lmf,lmb,rmf,rmb,echoL,echoR]
     pins = pins.Pins(i, o)
-    
-    
+
+# all the pins we will use
+lmf = 19
+lmb = 21
+rmf = 26
+rmb = 24
+sonar = 8
+trigL = 38
+echoL = 36
+trigR = 37
+echoR = 35
+
+setup()
