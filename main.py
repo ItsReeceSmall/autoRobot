22038dd80cde3fed820a7b1f88e18f5865ee08df
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-02 12:24:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import RPi.GPIO as gpio
 import time, os, sys
 import pins
 
-def setup(trigL, trigR, sonar, lmf, lmb, rmf, rmb, echoL, echoR):
+def setup():
     #set GPIO up
     gpio.setmode(gpio.BOARD)
     #Set up pins from a class
+    lmf = 19
+    lmb = 21
+    rmf = 26
+    rmb = 24
+    sonar = 8
+    trigL = 38
+    echoL = 36
+    trigR = 37
+    echoR = 35
+    # Set there categories
     inputs = [trigL,trigR]
     outputs = [sonar,lmf,lmb,rmf,rmb,echoL,echoR]
     print('### ATTEMPTING TO IMPORT AND SETUP PINS ###')
@@ -13,16 +23,7 @@ def setup(trigL, trigR, sonar, lmf, lmb, rmf, rmb, echoL, echoR):
     print('### ALL PINS IMPORTED AND SETUP SUCCESSFULLY ###')
 
 # all the pins we will use
-lmf = 19
-lmb = 21
-rmf = 26
-rmb = 24
-sonar = 8
-trigL = 38
-echoL = 36
-trigR = 37
-echoR = 35
 print ('pins are defined')
 print ('setup() is ran')
-setup(trigL, trigR, sonar, lmf, lmb, rmf, rmb, echoL, echoR)
+setup()
 gpio.cleanup()
