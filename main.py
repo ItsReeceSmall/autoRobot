53b0a8a8c12ee0707b1a9455cc84dcdf7f540532
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-02 11:27:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ import pins
 
 def setup(trigL,trigR,sonar,lmf,lmb,rmf,rmb,echoL,echoR):
     #set GPIO up
-    print ('pins are called in fine')
     gpio.setmode(gpio.BOARD)
-    print ('GPIO.BOARD is setup')
     #Set up pins from a class
-    print(' attempts to do i as array')
     i = [trigL,trigR]
-    print(' does i as an array thing')
-    print(' attempts the same with o')
     o = [sonar,lmf,lmb,rmf,rmb,echoL,echoR]
-    print('successfully does the same with o')
     print('attempts to define "pins" as the imported class Pins with arrays i and o in it')
-    pins = pins.Pins(i, o)
+    thepins = pins.Pins(i, o)
     print('success')
 
 # all the pins we will use
