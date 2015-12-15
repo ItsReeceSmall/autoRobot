523123a470d9f36bf7f44725a46f0f7149424a1c
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-15 10:25:52 +0000

    Update wheels.py

--- wheels.py
+++ wheels.py
@@ -2,21 +2,69 @@ import RPi.GPIO as gpio
 import time, sys, os
 
 class Wheels:
-  def __init__(self, wheelPins):
-    self.__wheelPins = wheelPins
+  def __init__(self, f, l, r, rmf, rmb, lmf, lmb):
+    self.__rmf = rmf
+    self.__rmb = rmb
+    self.__lmf = lmf
+    self.__lmb = lmb
+    self.__f = f
+    self.__l = l
+    self.__r = r
     self.wheelsGo()
     
   @property
-  def wheelPins(self):
-    return self.__wheelPins
-  @wheelPins.setter
-  def wheelPins(self, value):
-    self.__wheelPins = value
+  def rmf(self):
+    return self.__rmf
+  @rmf.setter
+  def rmf(self, value):
+    self.__rmf = value
   
+  @property
+  def rmb(self):
+    return self.__rmb
+  @rmb.setter
+  def rmb(self, value):
+    self.__rmb = value
+
+  @property
+  def lmf(self):
+    return self.__lmf
+  @lmf.setter
+  def lmf(self, value):
+    self.__lmf = value
+
+  @property
+  def lmb(self):
+    return self.__lmb
+  @lmb.setter
+  def lmb(self, value):
+    self.__lmb = value
+
+  @property
+  def f(self):
+    return self.__f
+  @f.setter
+  def f(self, value):
+    self.__f = value
+
+  @property
+  def l(self):
+    return self.__l
+  @l.setter
+  def l(self, value):
+    self.__l = value
+
+  @property
+  def r(self):
+    return self.__r
+  @r.setter
+  def r(self, value):
+    self.__r = value
+
   def wheelsGo(self):
     print ('#######################')
-    print ('Left: ' + str(wheelPins[5]))        # Prints the distance on screen to show what the pi is detecting
-    if wheelPins[5] < 25:
+    print ('Left: ' + str(l))        # Prints the distance on screen to show what the pi is detecting
+    if l < 25:
         lmfPWM.ChangeDutyCycle(65)
         print('')
         print ('lmf speed = 65')
@@ -30,8 +78,8 @@ class Wheels:
         print (' ')
         lmfPWM.ChangeDutyCycle(100)
         rmfPWM.ChangeDutyCycle(96)                    # If the distance is lower than 25cm then the code below is ran.
-    print ('Right: ' + str(wheelPins[6]))      # Prints the distance on screen to show what the pi is detecting
-    if wheelPins[6] < 25:
+    print ('Right: ' + str(r))      # Prints the distance on screen to show what the pi is detecting
+    if r < 25:
         lmfPWM.ChangeDutyCycle(100)
         print('')
         print ('lmf speed = 100')
@@ -44,13 +92,13 @@ class Wheels:
         print ('reset to 96')
         print (' ')
         rmfPWM.ChangeDutyCycle(96)                   # If the distance is lower than 25cm then the code below is ran.
-    print ('Front: ' + str(wheelPins[4]))      # Prints the distance on screen to show what the pi is detecting
-    if wheelPins[4] < 40:                      # If the front sensor is less than 40cm away from a block, it will run the code below
+    print ('Front: ' + str(f))      # Prints the distance on screen to show what the pi is detecting
+    if f < 40:                      # If the front sensor is less than 40cm away from a block, it will run the code below
         lmfPWM.ChangeDutyCycle(0)
         lmbPWM.ChangeDutyCycle(0)       # All the motors are stopped, set to 0
         rmfPWM.ChangeDutyCycle(0)
         rmbPWM.ChangeDutyCycle(0)
-        if wheelPins[5] < wheelPins[6]:                # Checks if the left distance is less than the right, if True, code below is ran
+        if l < r:                # Checks if the left distance is less than the right, if True, code below is ran
             print('Going Back')
             lmbPWM.ChangeDutyCycle(55)  # The wheels go backwards
             rmbPWM.ChangeDutyCycle(55) 
@@ -61,7 +109,7 @@ class Wheels:
             time.sleep(1.7)             # For 1.7 seconds
             lmbPWM.ChangeDutyCycle(0)   # Both wheels stop and are set to 0
             rmfPWM.ChangeDutyCycle(0)
-        if wheelPins[6] < wheelPins[5]:                # Checks if the right distance is less than the left, if True, code below is ran
+        if r < l:                # Checks if the right distance is less than the left, if True, code below is ran
             print('Going Back')
             lmbPWM.ChangeDutyCycle(55)  # The wheels go backwards
             rmbPWM.ChangeDutyCycle(55)
