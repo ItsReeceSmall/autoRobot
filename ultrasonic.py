f3015ae3de656e585e01f2a1c72ccb1e23b21131
aharris6798 <aharris@salisbury6c.ac.uk>
2015-12-15 10:40:50 +0000

    Update ultrasonic.py

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,14 +1,37 @@
-class ultrasonic:
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
 
-  def__init__(self):
-    self.__distance=0
+TRIG = 38
+ECHO = 37
 
-  @property
-    def Distance():
-      return self.__distance
+def getDistance
 
-  @distance.setter
-      def height((self, value):
-      self.__distance=value
+print "Distance Measurement In Progress"
 
-def 
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+GPIO.output(TRIG, False)
+print "Waiting For Sensor To Settle"
+time.sleep(2)
+
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO)==0:
+  pulse_start = time.time()
+
+while GPIO.input(ECHO)==1:
+  pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start
+
+distance = pulse_duration * 17150
+
+distance = round(distance, 2)
+
+print "Distance:",distance,"cm"
+
+GPIO.cleanup()
