c933e6c584b8fb8e91364024224e9e5687897ffc
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-15 10:50:05 +0000

    Update wheels.py

--- wheels.py
+++ wheels.py
@@ -97,63 +97,63 @@ class Wheels:
     print ('#######################')
     print ('Left: ' + str(self.__l))        # Prints the distance on screen to show what the pi is detecting
     if self.__l < 25:
-        lmfPWM.ChangeDutyCycle(65)
+        self.__lmfPWM.ChangeDutyCycle(65)
         print('')
         print ('lmf speed = 65')
-        lmbPWM.ChangeDutyCycle(0)
-        rmfPWM.ChangeDutyCycle(100)
+        self.__lmbPWM.ChangeDutyCycle(0)
+        self.__rmfPWM.ChangeDutyCycle(100)
         print ('rmf speed = 100')
         print(' ')
-        rmbPWM.ChangeDutyCycle(0)
+        self.__rmbPWM.ChangeDutyCycle(0)
         time.sleep(1)
         print ('reset to 100 and 96')
         print (' ')
-        lmfPWM.ChangeDutyCycle(100)
-        rmfPWM.ChangeDutyCycle(96)                    # If the distance is lower than 25cm then the code below is ran.
+        self.__lmfPWM.ChangeDutyCycle(100)
+        self.__rmfPWM.ChangeDutyCycle(96)                    # If the distance is lower than 25cm then the code below is ran.
     print ('Right: ' + str(self.__r))      # Prints the distance on screen to show what the pi is detecting
     if self.__r < 25:
-        lmfPWM.ChangeDutyCycle(100)
+        self.__lmfPWM.ChangeDutyCycle(100)
         print('')
         print ('lmf speed = 100')
-        lmbPWM.ChangeDutyCycle(0)
-        rmfPWM.ChangeDutyCycle(65)
+        self.__lmbPWM.ChangeDutyCycle(0)
+        self.__rmfPWM.ChangeDutyCycle(65)
         print ('rmf speed = 65')
         print('')
-        rmbPWM.ChangeDutyCycle(0)
+        self.__rmbPWM.ChangeDutyCycle(0)
         time.sleep(1)
         print ('reset to 96')
         print (' ')
-        rmfPWM.ChangeDutyCycle(96)                   # If the distance is lower than 25cm then the code below is ran.
+        self.__rmfPWM.ChangeDutyCycle(96)                   # If the distance is lower than 25cm then the code below is ran.
     print ('Front: ' + str(self.__f))      # Prints the distance on screen to show what the pi is detecting
     if self.__f < 40:                      # If the front sensor is less than 40cm away from a block, it will run the code below
-        lmfPWM.ChangeDutyCycle(0)
-        lmbPWM.ChangeDutyCycle(0)       # All the motors are stopped, set to 0
-        rmfPWM.ChangeDutyCycle(0)
-        rmbPWM.ChangeDutyCycle(0)
+        self.__lmfPWM.ChangeDutyCycle(0)
+        self.__lmbPWM.ChangeDutyCycle(0)       # All the motors are stopped, set to 0
+        self.__rmfPWM.ChangeDutyCycle(0)
+        self.__rmbPWM.ChangeDutyCycle(0)
         if self.__l < self.__r:                # Checks if the left distance is less than the right, if True, code below is ran
             print('Going Back')
-            lmbPWM.ChangeDutyCycle(55)  # The wheels go backwards
-            rmbPWM.ChangeDutyCycle(55) 
+            self.__lmbPWM.ChangeDutyCycle(55)  # The wheels go backwards
+            self.__rmbPWM.ChangeDutyCycle(55) 
             time.sleep(2)               # For 2 seconds
             print('Rotating Right')
-            lmbPWM.ChangeDutyCycle(53)
-            rmfPWM.ChangeDutyCycle(53)  # The Wheels rotate opposite ways to turn away from the block
+            self.__lmbPWM.ChangeDutyCycle(53)
+            self.__rmfPWM.ChangeDutyCycle(53)  # The Wheels rotate opposite ways to turn away from the block
             time.sleep(1.7)             # For 1.7 seconds
-            lmbPWM.ChangeDutyCycle(0)   # Both wheels stop and are set to 0
-            rmfPWM.ChangeDutyCycle(0)
+            self.__lmbPWM.ChangeDutyCycle(0)   # Both wheels stop and are set to 0
+            self.__rmfPWM.ChangeDutyCycle(0)
         if self.__r < self.__l:                # Checks if the right distance is less than the left, if True, code below is ran
             print('Going Back')
-            lmbPWM.ChangeDutyCycle(55)  # The wheels go backwards
-            rmbPWM.ChangeDutyCycle(55)
+            self.__lmbPWM.ChangeDutyCycle(55)  # The wheels go backwards
+            self.__rmbPWM.ChangeDutyCycle(55)
             time.sleep(2)               # For 2 seconds
             print('Rotating Left')
-            lmfPWM.ChangeDutyCycle(53)
-            rmbPWM.ChangeDutyCycle(53)  # The wheels rotate opposite ways to turn away from the block
+            self.__lmfPWM.ChangeDutyCycle(53)
+            self.__rmbPWM.ChangeDutyCycle(53)  # The wheels rotate opposite ways to turn away from the block
             time.sleep(1.7)             # For 1.7 seconds
-            lmfPWM.ChangeDutyCycle(0)   # Both wheels stop and are set to 0
-            rmbPWM.ChangeDutyCycle(0)
+            self.__lmfPWM.ChangeDutyCycle(0)   # Both wheels stop and are set to 0
+            self.__rmbPWM.ChangeDutyCycle(0)
         print('Normal Speed')
-        lmfPWM.ChangeDutyCycle(100)
-        rmfPWM.ChangeDutyCycle(96)     # Wheels return to normal speed before restarting the loop and checking for blockages again.
-        lmbPWM.ChangeDutyCycle(0)
-        rmbPWM.ChangeDutyCycle(0)
+        self.__lmfPWM.ChangeDutyCycle(100)
+        self.__rmfPWM.ChangeDutyCycle(96)     # Wheels return to normal speed before restarting the loop and checking for blockages again.
+        self.__lmbPWM.ChangeDutyCycle(0)
+        self.__rmbPWM.ChangeDutyCycle(0)
