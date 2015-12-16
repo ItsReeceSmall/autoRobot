666747278ab4f3f75a000e9864b5bea6323bcd12
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-16 12:49:32 +0000

    Update wheels.py

--- wheels.py
+++ wheels.py
@@ -2,7 +2,7 @@ import RPi.GPIO as gpio
 import time, sys, os
 
 class Wheels:
-  def __init__(self, rmf, rmb, lmf, lmb, f, l, r, i, lmfPWM, lmbPWM, rmfPWM, rmbPWM):
+  def __init__(self, rmf, rmb, lmf, lmb, f, l, r, i, lmfPWM, lmbPWM, rmfPWM, rmbPWM, irFL, irFR, irMID):
     self.__rmf = rmf
     self.__rmb = rmb
     self.__lmf = lmf
@@ -15,11 +15,11 @@ class Wheels:
     self.__rmbPWM = rmbPWM
     self.__lmfPWM = lmfPWM
     self.__lmbPWM = lmbPWM
-#    self.__irFL = irFL
-#    self.__irFR = irFR
-#    self.__irMID = irMID
+    self.__irFL = irFL
+    self.__irFR = irFR
+    self.__irMID = irMID
     self.wheelsGo()
-'''
+    
   @property
   def irFL(self):
     return self.__irFL
@@ -40,7 +40,7 @@ class Wheels:
   @irMID.setter
   def irMID(self, value):
     self.__irMID = value
-  '''
+
   @property
   def rmfPWM(self):
     return self.__rmfPWM
