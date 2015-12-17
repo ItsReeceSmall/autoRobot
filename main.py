9ea8d514307b4296abb5fce84dc19da353b217ae
Reece Small <rsmall@salisbury6c.ac.uk>
2015-12-17 14:45:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def setup():
         f = (thefront.Distance)
         l = (thesidesLeft.Distance)
         r = (thesidesRight.Distance)
-        irResponse = ir.irDetection(irFL, irFR, irMID)
+        irResponse = ir.irSensor(irFL, irFR, irMID)
         i = (irResponse.detection)
         print (i)
         thewheels = wheels.Wheels(rmf,rmb,lmf,lmb,f,l,r,i,lmfPWM,lmbPWM,rmfPWM,rmbPWM)
